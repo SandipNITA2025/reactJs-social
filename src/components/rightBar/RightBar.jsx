c15ed06284323cd263636d2ab4c6682e02a29ca8
SandpiVsCode <91713231+SandipNIT@users.noreply.github.com>
2022-12-12 22:53:40 +0530

    first commit

---
+++ src/components/rightBar/RightBar.jsx
@@ -0,0 +1,181 @@
+import React from "react";
+import "./rightbar.scss";
+import User from "../../assets/images/login3.jpg";
+
+const RightBar = () => {
+  return (
+    <div className="rightBar">
+      <div className="container">
+        <div className="item">
+          <span>Suggetions for you</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src={User} alt="" />
+              <span>Mili Leo</span>
+            </div>
+            <div className="buttons">
+              <button>Follow</button>
+              <button>Dismiss</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={User} alt="" />
+              <span>Mili Leo</span>
+            </div>
+            <div className="buttons">
+              <button>Follow</button>
+              <button>Dismiss</button>
+            </div>
+          </div>
+        </div>
+
+        <hr />
+
+        <div className="item">
+          <span>Latest Activities</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src={User} alt="" />
+              <p>
+                <span>Mili Leo</span>
+                Shared your post
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={User} alt="" />
+              <p>
+                <span>Mili Leo</span>
+                Shared your post
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+        </div>
+
+        <hr />
+
+        <div className="item">
+          <span>Online Friends</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src={User} alt="" />
+              <div className="dot"></div>
+              <span>Mili Leo</span>
+            </div>
+              <span>Online</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={User} alt="" />
+              <div className="dot"></div>
+              <span>Mili Leo</span>
+            </div>
+              <span>Online</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={User} alt="" />
+              <div className="dot"></div>
+              <span>Mili Leo</span>
+            </div>
+              <span>Online</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={User} alt="" />
+              <div className="dot"></div>
+              <span>Mili Leo</span>
+            </div>
+              <span>Online</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={User} alt="" />
+              <div className="dot"></div>
+              <span>Mili Leo</span>
+            </div>
+              <span>Online</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={User} alt="" />
+              <div className="dot"></div>
+              <span>Mili Leo</span>
+            </div>
+              <span>Online</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={User} alt="" />
+              <div className="dot"></div>
+              <span>Mili Leo</span>
+            </div>
+              <span>Online</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={User} alt="" />
+              <div className="dot"></div>
+              <span>Mili Leo</span>
+            </div>
+              <span>Online</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={User} alt="" />
+              <div className="dot"></div>
+              <span>Mili Leo</span>
+            </div>
+              <span>Online</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={User} alt="" />
+              <div className="dot"></div>
+              <span>Mili Leo</span>
+            </div>
+              <span>Online</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={User} alt="" />
+              <div className="dot"></div>
+              <span>Mili Leo</span>
+            </div>
+              <span>Online</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={User} alt="" />
+              <div className="dot"></div>
+              <span>Mili Leo</span>
+            </div>
+              <span>Online</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={User} alt="" />
+              <div className="dot"></div>
+              <span>Mili Leo</span>
+            </div>
+              <span>Online</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={User} alt="" />
+              <div className="dot"></div>
+              <span>Mili Leo</span>
+            </div>
+              <span>Online</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RightBar;
---
+++ src/data/postsData.jsx
@@ -0,0 +1,41 @@
+const postsData = [
+  {
+    id: 1,
+    userId: 1,
+    name: "John Welly",
+    contentPic:
+      "https://i.postimg.cc/XNxbhCqx/futuristic-14-920x518.jpg",
+    profilePic:
+      "https://i.postimg.cc/mZP646wP/2289-Sk-VNQSBGQU1-PIDEw-Mjgt-MTIy.jpg",
+    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+    cover: "https://images.unsplash.com/photo-1566994616736-69fe8cc420f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1086&q=80",
+    place: 'USA',
+    lan: "Engligh",
+  },
+  {
+    id: 2,
+    userId: 2,
+    name: "Luccy",
+    contentPic:
+      "https://i.postimg.cc/90Xj0W8G/anime-girl-smile-23-920x518.jpg",
+    profilePic: "https://i.postimg.cc/6Q8c9d5w/german.png",
+    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+    cover: "https://images.unsplash.com/photo-1434725039720-aaad6dd32dfe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1042&q=80",
+    place: "Las Vegas",
+    lan: "Engligh",
+  },
+  {
+    id: 3,
+    userId: 3,
+    name: "Peter Leo",
+    contentPic:
+      "https://i.postimg.cc/zvdYR964/fantasy-traveler-14-920x518.jpg",
+    profilePic: "https://i.postimg.cc/W3r7ZTkH/diver.png",
+    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+    cover: "https://images.unsplash.com/photo-1506869640319-fe1a24fd76dc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    place: "London",
+    lan: "Engligh",
+  },
+];
+
+export default postsData;
