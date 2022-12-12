234465fd91955da2b00a5f1f68f1f69177a957fc
SandpiVsCode <91713231+SandipNIT@users.noreply.github.com>
2022-12-12 23:35:40 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -67,14 +67,14 @@ const App = () => {
       ],
     },
 
-    {
-      path: "/login",
-      element: <Login />,
-    },
-    {
-      path: "/register",
-      element: <Register />,
-    },
+    // {
+    //   path: "/login",
+    //   element: <Login />,
+    // },
+    // {
+    //   path: "/register",
+    //   element: <Register />,
+    // },
   ]);
 
   return (
